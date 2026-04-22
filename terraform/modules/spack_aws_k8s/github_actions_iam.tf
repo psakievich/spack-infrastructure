7f415235c85b9685fd28b615353dadbe7219b09d
Jacob Nesbitt <jjnesbitt2@gmail.com>
2026-04-22 16:51:04 -0400

    Allow github actions to put objects to source mirror

--- terraform/modules/spack_aws_k8s/github_actions_iam.tf
+++ terraform/modules/spack_aws_k8s/github_actions_iam.tf
@@ -74,3 +74,60 @@ resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
   role       = aws_iam_role.github_actions_readonly[0].name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+
+# Allow github actions run from the develop branch of spack/spack to put objects into the source mirror
+resource "aws_iam_role" "github_actions_put_to_source_mirror" {
+  count = var.deployment_name == "prod" ? 1 : 0
+
+  name = "GitHubLLNLSourceMirror"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions[0].arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:spack/spack:ref:refs/heads/develop"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_put_to_source_mirror" {
+  count = var.deployment_name == "prod" ? 1 : 0
+
+  name = "PutToSpackLLNLSourceMirror"
+  role = aws_iam_role.github_actions_put_to_source_mirror[0].name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid      = "PutToSpackLLNLSourceMirror"
+        Effect   = "Allow"
+        Action   = "s3:PutObject"
+        Resource = "arn:aws:s3:::spack-llnl-mirror/_source-cache/*"
+      },
+      {
+        Sid      = "ListSpackLLNLSourceMirror"
+        Effect   = "Allow"
+        Action   = "s3:ListBucket"
+        Resource = "arn:aws:s3:::spack-llnl-mirror"
+        Condition = {
+          StringLike = {
+            "s3:prefix" = ["_source-cache/*"]
+          }
+        }
+      }
+    ]
+  })
+}
